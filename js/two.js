3450d5d284f5450224931043ddc7ee8a8ec235a0
Jozef <jozefw@sbcglobal.net>
2019-07-07 21:22:40 -0700

    first commit

---
+++ js/two.js
@@ -0,0 +1,4 @@
+var p = document.createElement('p');
+p.innerText = "Second";
+p.setAttribute('id','second');
+document.body.appendChild(p);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+require("../js/one.js");
+require("../js/two.js");
+require("../js/three.js");
+require("../css/main.css");
